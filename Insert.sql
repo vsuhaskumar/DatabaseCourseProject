323995ef0d2ddb32fed88e08ee2a9326fcef8997
weiiew <ben_weiiew@qq.com>
2017-05-03 17:36:55 -0500

    Inerst2

---
+++ Insert.sql
@@ -0,0 +1,246 @@
+
+INSERT INTO Students VALUES ('apple@uiowa.edu', 'Apple', '1992-07-20', 'Florida', 'M');
+INSERT INTO Students VALUES ('kathy@gmail.com', 'Kathy', '1990-03-01', 'New Jersey', 'F');
+INSERT INTO Students VALUES ('brown@hotmail.com', 'Brown', '1983-12-11', 'New York', 'M');
+INSERT INTO Students VALUES ('cathy@gmail.com', 'Cathy', '1992-03-10', 'Alaska', 'F');
+INSERT INTO Students VALUES ('wells@gmail.com', 'Wells', '1993-12-30', 'Illinois', 'M');
+INSERT INTO Students VALUES ('bob@yahoo.com', 'Bob', '1976-05-15', 'Florida', 'F');
+INSERT INTO Students VALUES ('william@gmail.com', 'William', '1985-04-02', 'New Mexico', 'M');
+INSERT INTO Students VALUES ('booth@gmail.com', 'Booth', '2002-07-02', 'Florida', 'F');
+INSERT INTO Students VALUES ('peter@gmail.com', 'Peter', '1991-03-03', 'Iowa', 'M');
+INSERT INTO Students VALUES ('daniel@uiowa.com', 'Daniel', '2000-11-11', 'New York', 'M');
+INSERT INTO Students VALUES ('kelly@uiowa.com', 'Kelly', '1994-12-03', 'Iowa', 'F');
+
+
+
+
+
+INSERT INTO Courses VALUES (1, 'Elements of photography', 'This course will teach you about photography', '16 weeks', 'easy', 'Arts', Q:Does it require a camera? A:Yes);
+INSERT INTO Courses VALUES (2, 'Investment Management', 'This course will teach you about Investing', '20 weeks', 'medium', 'Finance', 'Q: Is it useful? A: Yes.');
+INSERT INTO Courses VALUES (3, 'Financial Accounting', 'This course will teach you about Financial Accounting', '15 weeks', 'hard', 'Finance', 'Q: Is it a online course? A: No');
+INSERT INTO Courses VALUES (4, 'Intro to Management', 'This course is an intro to management', '10 weeks', 'easy', 'Management', 'Q: Is it needs special permit? A: No');
+INSERT INTO Courses VALUES (5, 'Intro to Law', 'This course will teach you about intro to law', '8 weeks', 'easy', 'Law', 'Q: What is it aiming for? A: To help build basic knowledge of law');
+INSERT INTO Courses VALUES (6, 'Basic Chemistry', 'This course will teach you about basic chemistry', '8 weeks', 'easy', 'Chemistry', 'Q:Is it interesting? A: Yes');
+INSERT INTO Courses VALUES (7, 'Ballroom Dancing', 'This course will teach you about ballroom dancing', '8 weeks', 'medium', 'Arts', 'Q: What is dancing? A: Move your body');
+INSERT INTO Courses VALUES (8, 'Statistics', 'This course will teach you about Statistics', '10 weeks', 'medium', 'Mathematics', 'Q: Will we need a calculator? A: Yes');
+INSERT INTO Courses VALUES (9, 'Marco Economics', 'This course will teach you about macro economics', '12 weeks', 'medium', 'Economy', NULL);
+INSERT INTO Courses VALUES (10, 'Operating System', 'This course will teach you about operating system', '6 weeks', 'hard', 'Computer Sciences', NULL);
+INSERT INTO Courses VALUES (11, 'Commercial Banking', 'This course will teach you about commercial banking', '14 weeks', 'medium', 'Business', 'Q: Will I learn how to store money? A: Yes');
+
+
+
+INSERT INTO Universities VALUES (1, 'University of Illinois', 'A set of school');
+INSERT INTO Universities VALUES (2, 'Brandeis', 'Located near the Boston');
+INSERT INTO Universities VALUES (3, 'NYU', 'Good business school');
+INSERT INTO Universities VALUES (4, 'Columbia University', 'Famous school located in New York');
+INSERT INTO Universities VALUES (5, 'North Eastern University', 'Located in Chicago');
+INSERT INTO Universities VALUES (6, 'University of Miami', 'Located in Miami');
+INSERT INTO Universities VALUES (7, 'University of Texas - Austin', 'Very good in business school');
+INSERT INTO Universities VALUES (8, 'UCSD', 'Located in San Tiago, California');
+INSERT INTO Universities VALUES (9, 'Eastern Iowa ', 'Community school in Iowa');
+INSERT INTO Universities VALUES (10, 'Princeton', 'Best school for math');
+
+
+
+INSERT INTO Instructors VALUES (1, 'Eddie Jessup', 'F', 'Mathematics');
+INSERT INTO Instructors VALUES (2, 'Robert Langdon', 'M', 'Economy');
+INSERT INTO Instructors VALUES (3, 'William Dyer', 'M', 'Computer Science');
+INSERT INTO Instructors VALUES (4, 'Pippy Peepee Poopypants', 'M', 'Psychology');
+INSERT INTO Instructors VALUES (5, 'Gervase Fen', 'M', 'Computer Science');
+INSERT INTO Instructors VALUES (6, 'Digory Kirke', 'F', 'Chemistry');
+INSERT INTO Instructors VALUES (7, 'James Dunworthy', 'M', 'Mathematics');
+INSERT INTO Instructors VALUES (8, 'Abraham Van Helsing', 'F', 'Arts');
+INSERT INTO Instructors VALUES (9, 'Collins', 'F', 'Finance');
+INSERT INTO Instructors VALUES (10, 'Hari Seldon', 'M', 'Management');
+INSERT INTO Instructors VALUES (11, 'Dors Venabili', 'M', 'Law');
+INSERT INTO Instructors VALUES (12, 'Yugo Amaryl', 'F', 'Computer Science');
+INSERT INTO Instructors VALUES (13, 'Cuthbert Binns', 'M', 'Mathematics');
+INSERT INTO Instructors VALUES (14, 'Charity Burbage', 'M', 'Finance');
+INSERT INTO Instructors VALUES (15, 'Alecto Carrow', 'M', 'Economy');
+
+
+
+INSERT INTO Transactions VALUES (1, '2017-01-11 09:21:31', 'Success');
+INSERT INTO Transactions VALUES (2, '2017-01-16 02:09:52', 'Success');
+INSERT INTO Transactions VALUES (3, '2017-01-16 08:40:15', 'Success');
+INSERT INTO Transactions VALUES (4, '2017-02-05 07:31:31', 'Success');
+INSERT INTO Transactions VALUES (5, '2017-02-14 10:21:10', 'Success');
+INSERT INTO Transactions VALUES (6, '2017-02-15 11:15:30', 'Failure');
+INSERT INTO Transactions VALUES (7, '2017-02-17 11:16:51', 'Success');
+INSERT INTO Transactions VALUES (8, '2017-02-21 06:12:41', 'Failure');
+INSERT INTO Transactions VALUES (9, '2017-02-21 06:18:25', 'Success');
+INSERT INTO Transactions VALUES (10, '2017-02-22 05:12:21', 'Success');
+INSERT INTO Transactions VALUES (11, '2017-02-22 01:31:51', 'Success');
+INSERT INTO Transactions VALUES (12, '2017-02-22 03:53:21', 'Success');
+INSERT INTO Transactions VALUES (13, '2017-03-12 05:06:21', 'Success');
+INSERT INTO Transactions VALUES (14, '2017-03-12 06:32:21', 'Failure');
+INSERT INTO Transactions VALUES (15, '2017-03-15 05:13:31', 'Success');
+INSERT INTO Transactions VALUES (16, '2017-03-16 01:52:21', 'Success');
+INSERT INTO Transactions VALUES (17, '2017-04-11 12:01:35', 'Success');
+INSERT INTO Transactions VALUES (18, '2017-04-12 11:12:24', 'Success');
+INSERT INTO Transactions VALUES (19, '2017-04-16 03:15:45', 'Failure');
+INSERT INTO Transactions VALUES (20, '2017-04-16 13:12:45', 'Success');
+INSERT INTO Transactions VALUES (21, '2017-04-22 15:35:15', 'Success');
+INSERT INTO Transactions VALUES (22, '2017-04-25 17:55:15', 'Success');
+INSERT INTO Transactions VALUES (23, '2017-04-26 17:42:15', 'Success');
+
+
+
+INSERT INTO CreditCards VALUES (2315431563163163, '01/20', 'Apple Juice', 163);
+INSERT INTO CreditCards VALUES (2346243514233131, '11/20', 'Kathy Tu', 213);
+INSERT INTO CreditCards VALUES (3143151631623245, '08/18', 'Brown Meller', 163);
+INSERT INTO CreditCards VALUES (1987192378951892, '05/18', 'Cathy Ruler', 234);
+INSERT INTO CreditCards VALUES (9821470983517109, '11/16', 'Wells Magof', 374);
+INSERT INTO CreditCards VALUES (2139750912589218, '12/27', 'Bob Winner', 136);
+INSERT INTO CreditCards VALUES (2137891384578512, '01/22', 'William Wuger', 643);
+INSERT INTO CreditCards VALUES (2137981257892189, '11/19', 'Booth Rocker', 463);
+INSERT INTO CreditCards VALUES (0320321093218321, '03/21', 'Peter Dough', 724);
+INSERT INTO CreditCards VALUES (2139080519i09210, '04/20', 'Daniel Bendict', 537);
+INSERT INTO CreditCards VALUES (2132315431532646, '01/20', 'Kelly High', 537);
+
+
+
+INSERT INTO Ratings VALUES ('apple@uiowa.edu', 1, 4, 'Good Course');
+INSERT INTO Ratings VALUES ('brown@hotmail.com', 2, 2, 'Bad Instructor');
+INSERT INTO Ratings VALUES ('booth@gmail.com', 5, 5, 'Useful');
+INSERT INTO Ratings VALUES ('daniel@uiowa.com', 10, 3, 'Not too bad');
+INSERT INTO Ratings VALUES ('daniel@uiowa.com', 7, 2, 'Below average course');
+INSERT INTO Ratings VALUES ('william@gmail.com', 2, 4, 'Learn a lot');
+INSERT INTO Ratings VALUES ('cathy@gmail.com', 9, 3, NULL);
+INSERT INTO Ratings VALUES ('bob@yahoo.com', 7, 4, Null);
+INSERT INTO Ratings VALUES ('kelly@uiowa.com', 8, 5, 'Recommended');
+INSERT INTO Ratings VALUES ('peter@gmail.com', 11, 4, 'Interesting);
+
+
+
+INSERT INTO UnivCourses VALUES (1, 7);
+INSERT INTO UnivCourses VALUES (2, 1);
+INSERT INTO UnivCourses VALUES (3, 6);
+INSERT INTO UnivCourses VALUES (3, 11);
+INSERT INTO UnivCourses VALUES (4, 5);
+INSERT INTO UnivCourses VALUES (5, 8);
+INSERT INTO UnivCourses VALUES (6, 2);
+INSERT INTO UnivCourses VALUES (7, 4);
+INSERT INTO UnivCourses VALUES (8, 3);
+INSERT INTO UnivCourses VALUES (9, 10);
+INSERT INTO UnivCourses VALUES (10, 9);
+
+
+
+INSERT INTO UnivInstructors VALUES (1, 10);
+INSERT INTO UnivInstructors VALUES (1, 2);
+INSERT INTO UnivInstructors VALUES (2, 5);
+INSERT INTO UnivInstructors VALUES (6, 11);
+INSERT INTO UnivInstructors VALUES (3, 6);
+INSERT INTO UnivInstructors VALUES (4, 8);
+INSERT INTO UnivInstructors VALUES (5, 4);
+INSERT INTO UnivInstructors VALUES (5, 7);
+INSERT INTO UnivInstructors VALUES (9, 1);
+INSERT INTO UnivInstructors VALUES (10, 3);
+INSERT INTO UnivInstructors VALUES (7, 9);
+
+
+
+
+INSERT TABLE CourseInstructors VALUES (1, 8);
+INSERT TABLE CourseInstructors VALUES (2, 14);
+INSERT TABLE CourseInstructors VALUES (3, 9);
+INSERT TABLE CourseInstructors VALUES (4, 10);
+INSERT TABLE CourseInstructors VALUES (5, 11);
+INSERT TABLE CourseInstructors VALUES (6, 6);
+INSERT TABLE CourseInstructors VALUES (7, 8);
+INSERT TABLE CourseInstructors VALUES (8, 1);
+INSERT TABLE CourseInstructors VALUES (9, 15);
+INSERT TABLE CourseInstructors VALUES (10, 12);
+INSERT TABLE CourseInstructors VALUES (11, 2);
+
+
+
+INSERT INTO StudentCourses VALUES ('apple@uiowa.edu', 1);
+INSERT INTO StudentCourses VALUES ('kathy@gmail.com', 11);
+INSERT INTO StudentCourses VALUES ('kathy@gmail.com', 2);
+INSERT INTO StudentCourses VALUES ('brown@hotmail.com', 6);
+INSERT INTO StudentCourses VALUES ('brown@hotmail.com', 3);
+INSERT INTO StudentCourses VALUES ('brown@hotmail.com', 7);
+INSERT INTO StudentCourses VALUES ('cathy@gmail.com', 3);
+INSERT INTO StudentCourses VALUES ('cathy@gmail.com', 8);
+INSERT INTO StudentCourses VALUES ('wells@gmail.com', 9);
+INSERT INTO StudentCourses VALUES ('wells@gmail.com', 10);
+INSERT INTO StudentCourses VALUES ('bob@yahoo.com', 2);
+INSERT INTO StudentCourses VALUES ('bob@yahoo.com', 3);
+INSERT INTO StudentCourses VALUES ('william@gmail.com', 6);
+INSERT INTO StudentCourses VALUES ('william@gmail.com', 1);
+INSERT INTO StudentCourses VALUES ('booth@gmail.com', 4);
+INSERT INTO StudentCourses VALUES ('booth@gmail.com', 2);
+INSERT INTO StudentCourses VALUES ('peter@gmail.com', 6);
+INSERT INTO StudentCourses VALUES ('daniel@uiowa.com', 9);
+INSERT INTO StudentCourses VALUES ('daniel@uiowa.com', 10);
+INSERT INTO StudentCourses VALUES ('kelly@uiowa.com', 7);
+
+
+
+INSERT INTO StudentCourseTrans VALUES ('apple@uiowa.edu', 1, 1);
+INSERT INTO StudentCourseTrans VALUES ('kathy@gmail.com', 11, 2);
+INSERT INTO StudentCourseTrans VALUES ('kathy@gmail.com', 2, 3);
+INSERT INTO StudentCourseTrans VALUES ('brown@hotmail.com', 6, 4);
+INSERT INTO StudentCourseTrans VALUES ('brown@hotmail.com', 3, 5);
+INSERT INTO StudentCourseTrans VALUES ('brown@hotmail.com', 7, 6);
+INSERT INTO StudentCourseTrans VALUES ('brown@hotmail.com', 7, 7);
+INSERT INTO StudentCourseTrans VALUES ('cathy@gmail.com', 3, 8);
+INSERT INTO StudentCourseTrans VALUES ('cathy@gmail.com', 3, 9);
+INSERT INTO StudentCourseTrans VALUES ('cathy@gmail.com', 8, 10);
+INSERT INTO StudentCourseTrans VALUES ('wells@gmail.com', 9, 11);
+INSERT INTO StudentCourseTrans VALUES ('wells@gmail.com', 10, 12);
+INSERT INTO StudentCourseTrans VALUES ('bob@yahoo.com', 2, 13);
+INSERT INTO StudentCourseTrans VALUES ('bob@yahoo.com', 3, 14);
+INSERT INTO StudentCourseTrans VALUES ('bob@yahoo.com', 3, 15);
+INSERT INTO StudentCourseTrans VALUES ('william@gmail.com', 6, 16);
+INSERT INTO StudentCourseTrans VALUES ('william@gmail.com', 1, 17);
+INSERT INTO StudentCourseTrans VALUES ('booth@gmail.com', 4, 18);
+INSERT INTO StudentCourseTrans VALUES ('booth@gmail.com', 2, 19);
+INSERT INTO StudentCourseTrans VALUES ('peter@gmail.com', 6, 20);
+INSERT INTO StudentCourseTrans VALUES ('daniel@uiowa.com', 9, 21);
+INSERT INTO StudentCourseTrans VALUES ('daniel@uiowa.com', 10, 22);
+INSERT INTO StudentCourseTrans VALUES ('kelly@uiowa.com', 7, 23);
+
+
+
+
+INSERT INTO StudentCards VALUES ('apple@uiowa.edu', 2315431563163163);
+INSERT INTO StudentCards VALUES ('kathy@gmail.com', 2346243514233131);
+INSERT INTO StudentCards VALUES ('brown@hotmail.com', 3143151631623245);
+INSERT INTO StudentCards VALUES ('cathy@gmail.com', 1987192378951892);
+INSERT INTO StudentCards VALUES ('wells@gmail.com', 9821470983517109);
+INSERT INTO StudentCards VALUES ('bob@yahoo.com', 2139750912589218);
+INSERT INTO StudentCards VALUES ('william@gmail.com', 2137891384578512);
+INSERT INTO StudentCards VALUES ('booth@gmail.com', 2137981257892189);
+INSERT INTO StudentCards VALUES ('peter@gmail.com', 2139080519i09210);
+INSERT INTO StudentCards VALUES ('daniel@uiowa.com', 2139080519i09210);
+INSERT INTO StudentCards VALUES ('kelly@uiowa.com', 2132315431532646);
+
+
+
+INSERT INTO CardTrans VALUES (2315431563163163, 1);
+INSERT INTO CardTrans VALUES (2346243514233131, 2);
+INSERT INTO CardTrans VALUES (2346243514233131, 3);
+INSERT INTO CardTrans VALUES (3143151631623245, 4);
+INSERT INTO CardTrans VALUES (3143151631623245, 5);
+INSERT INTO CardTrans VALUES (3143151631623245, 6);
+INSERT INTO CardTrans VALUES (3143151631623245, 7);
+INSERT INTO CardTrans VALUES (3143151631623245, 8);
+INSERT INTO CardTrans VALUES (1987192378951892, 9);
+INSERT INTO CardTrans VALUES (1987192378951892, 10);
+INSERT INTO CardTrans VALUES (9821470983517109, 11);
+INSERT INTO CardTrans VALUES (9821470983517109, 12);
+INSERT INTO CardTrans VALUES (2139750912589218, 13);
+INSERT INTO CardTrans VALUES (2139750912589218, 14);
+INSERT INTO CardTrans VALUES (2139750912589218, 15);
+INSERT INTO CardTrans VALUES (2137891384578512, 16);
+INSERT INTO CardTrans VALUES (2137891384578512, 17);
+INSERT INTO CardTrans VALUES (2137981257892189, 18);
+INSERT INTO CardTrans VALUES (2137981257892189, 19);
+INSERT INTO CardTrans VALUES (2139080519i09210, 20);
+INSERT INTO CardTrans VALUES (2139080519i09210, 21);
+INSERT INTO CardTrans VALUES (2139080519i09210, 22);
+INSERT INTO CardTrans VALUES (2132315431532646, 22);
+
